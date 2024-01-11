23bd59bf43092bf181675c450baa8c13d9ff8d40
Leusin <leusin@naver.com>
2024-01-11 10:36:37 +0900

    Add make_is_array example

--- examples/examples/main.cpp
+++ examples/examples/main.cpp
@@ -3,7 +3,35 @@
 
 #include <iostream>
 
+template<typename T>
+struct is_array_custom
+{
+  static constexpr bool value = false;
+  static constexpr size_t size = -1;
+};
+
+template <typename T, size_t N> 
+struct is_array_custom < T[N] >
+{
+  static constexpr bool value = true;
+  static constexpr size_t size = N;
+};
+
+template <typename T>
+void foo(T& a)
+{
+    if (is_array_custom<T>::value)
+    {
+      std::cout << "size of array : " << is_array_custom<T>::size << std::endl;
+    }
+}
+
+
 int main() 
 {
+    int x[3] = {1, 2, 3};
+
+    foo(x);
+
   return 0;
 }
