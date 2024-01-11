d35aca8b4d901d117cf81ab6577e1575592f4dc4
Leusin <leusin@naver.com>
2024-01-11 13:02:15 +0900

    Add simple_cincepts

---
+++ examples/examples/simple_concepts.cpp
@@ -0,0 +1,21 @@
+/// concept ½Ç½À
+///
+/// 2024-01-11
+
+#include <concepts>
+#include <iostream>
+
+template <typename T>
+concept GT4 = sizeof(T) >= 4;
+
+template <typename T>
+  requires GT4<T>
+void foo(T a) {}
+
+int main() {
+  foo(short()); // error
+  foo(int());
+  foo(long());
+
+  return 0;
+}
